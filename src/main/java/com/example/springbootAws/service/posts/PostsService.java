41efe1f84800b4e94a5f82c9898df9da59aeabb7
jinhyeon <vmflwu123@gmail.com>
2022-07-05 21:29:58 +0900

    포스트 등록 기능 생성

---
+++ src/main/java/com/example/springbootAws/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.example.springbootAws.service.posts;
+
+import com.example.springbootAws.domain.posts.PostsRepository;
+import com.example.springbootAws.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
